065822916024318242acd1e3920cab745645e338
abhinavkolli03 <abhinavkolli03@gmail.com>
2024-05-31 02:19:41 -0500

    fixed nativewind, assets, new components, and tabs structure with intro page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const profile = () => {
+const SignIn = () => {
   return (
     <View>
-      <Text>profile</Text>
+      <Text>SignIn</Text>
     </View>
   )
 }
 
-export default profile
+export default SignIn
 
 const styles = StyleSheet.create({})
